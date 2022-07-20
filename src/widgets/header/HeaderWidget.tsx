e55c082ac6ed0f89bd659e34e25fa48f37098098
Diaa Al kabbani <49997074+diaakabbani@users.noreply.github.com>
2022-07-20 12:51:52 +0400

    diaa header

--- src/widgets/header/HeaderWidget.tsx
+++ src/widgets/header/HeaderWidget.tsx
@@ -1,15 +1,24 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import React from "react";
+import { Container, Image, Nav, Navbar } from "react-bootstrap";
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 import "./header-style.scss";
 export const HeaderWidget = () => {
   return (
     <div id="header-style">
-      <Navbar  expand="lg">
+      <Navbar  expand="sm">
         <Container fluid>
           <Navbar.Brand >Boss Platform</Navbar.Brand>
+          {/* <NavbarCollapse id="basic-navbar-nav">
+            <nav className="ms-auto">
+              <Nav.Link href="#mohamed almehdi">mmohamed almehdi</Nav.Link>
+            </nav>
+          </NavbarCollapse> */}
           <Navbar.Toggle />
           <Navbar.Collapse>
-            <Nav className="me-auto">
-              
+            <Nav className="ms-auto">
+
+              <a href="/#"  >mohamed Almehdi</a>
+              <Image thumbnail style={{height:30, background:'none' , objectFit:'contain'}} src="assets/icons/icons8-user-24.png"/>
             </Nav>
           </Navbar.Collapse>
         </Container>
